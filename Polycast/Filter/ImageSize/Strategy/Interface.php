a699e0503420da35216146b1e7ead19cedd49362
Andreas Baumgart <polycaster@polycast.de>
2009-12-17 21:45:45 +0100

    Changed directory structure and added a stub for upcoming test cases.

---
+++ Polycast/Filter/ImageSize/Strategy/Interface.php
@@ -0,0 +1,13 @@
+<?php
+
+interface Polycast_Filter_ImageSize_Strategy_Interface
+{
+    /**
+     * Return canvas resized according to the given dimensions.
+     * @param resource $image GD image resource
+     * @param int $width Output width
+     * @param int $height Output height
+     * @return resource GD image resource
+     */
+    public function resize($image, $width, $height);
+}
