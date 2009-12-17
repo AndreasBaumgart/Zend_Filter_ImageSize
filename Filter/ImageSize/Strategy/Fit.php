e8b02ffd4b3f09415417d20bebc6c9a4bef3938a
Andreas Baumgart <polycaster@polycast.de>
2009-12-17 21:33:56 +0100

    Initial commit

---
+++ Filter/ImageSize/Strategy/Fit.php
@@ -0,0 +1,42 @@
+<?php
+
+/** @var Polycast_Filter_ImageSize_Strategy_Interface */
+require_once 'Polycast/Filter/ImageSize/Strategy/Interface.php';
+
+/**
+ * Strategy for resizing the image by fitting the content into the given 
+ * dimensions.
+ */
+class Polycast_Filter_Imagesize_Strategy_Fit 
+    implements Polycast_Filter_ImageSize_Strategy_Interface
+{
+    /**
+     * Return canvas resized according to the given dimensions.
+     * @param resource $image GD image resource
+     * @param int $width Output width
+     * @param int $height Output height
+     * @return resource GD image resource
+     */
+    public function resize($image, $width, $height)
+    {
+        $origWidth = imageSX($image);
+        $origHeight = imageSY($image);
+        
+        $rWidth = ceil($origWidth / $width);
+        $rHeight = ceil($origHeight / $height);
+
+        if($rWidth > $rHeight) {
+            $ratio = $rWidth;
+        } else {
+            $ratio = $rHeight;
+        }
+        
+        $newWidth = $origWidth / $ratio;
+        $newHeight = $origHeight / $ratio;    
+        
+        $resized = imageCreateTrueColor($newWidth, $newHeight);
+        imageCopyResampled($resized, $image, 0, 0, 0, 0, $newWidth, $newHeight, $origWidth, $origHeight);
+        
+        return $resized;
+    }
+}
