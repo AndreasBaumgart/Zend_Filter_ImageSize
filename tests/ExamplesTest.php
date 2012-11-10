ed4d15b0bd2379a8eb2f942d191cfa961ecef49f
Andreas Baumgart <andreas@polycast.de>
2012-11-10 23:29:16 +0100

    Added test case with usage examples.

--- tests/ExamplesTest.php
+++ tests/ExamplesTest.php
@@ -1,9 +1,82 @@
 <?php
 
-class TestTest extends PHPUnit_Framework_TestCase
+class ExamplesTest extends PHPUnit_Framework_TestCase
 {
-    public function testTest()
+    public function tearDown() {
+        parent::tearDown();
+        
+        foreach (glob(TESTING_TMP_DIR . '/*.jpg') as $obsoleteJpegFile) {
+            @unlink($obsoleteJpegFile);
+        }
+    }
+    
+    public function testExample01_FitImageIntoABox()
     {
-        $this->assertEquals(1, 1);
+        $inputPath = TESTING_ASSETS_DIR . '/rick.jpg'; 
+        $outputDirectory = TESTING_TMP_DIR; 
+        
+        $filter = new Polycast_Filter_ImageSize(); 
+        $filter->setThumnailDirectory($outputDirectory)
+               ->setWidth(100)
+               ->setHeight(100)
+               ->setQuality(50)
+               ->setStrategy(new Polycast_Filter_ImageSize_Strategy_Fit())
+               ->setOverwriteMode(Polycast_Filter_ImageSize::OVERWRITE_ALL)
+        ; 
+        $outputPath = $filter->filter($inputPath); 
+        
+        $this->assertEquals(true, is_file($outputPath));
+        $this->assertImageFitsInBox(100, 100, $outputPath);
+    }
+    
+    public function testExample01_CropImageToFitIntoABox()
+    {
+        $inputPath = TESTING_ASSETS_DIR . '/rick.jpg'; 
+        $outputDirectory = TESTING_TMP_DIR; 
+        
+        $filter = new Polycast_Filter_ImageSize(); 
+        $filter->setThumnailDirectory($outputDirectory)
+               ->setWidth(100)
+               ->setHeight(100)
+               ->setQuality(50)
+               ->setStrategy(new Polycast_Filter_ImageSize_Strategy_Crop())
+               ->setOverwriteMode(Polycast_Filter_ImageSize::OVERWRITE_ALL)
+        ; 
+        $outputPath = $filter->filter($inputPath); 
+        
+        $this->assertEquals(true, is_file($outputPath));
+        $this->assertImageSizeEquals(100, 100, $outputPath);        
+    }
+    
+    public function assertImageFitsInBox($expectedWidth, $expectedHeight, $imagePath)
+    {
+        list($actualWidth, $actualHeight) = $this->getImageSize($imagePath);
+        
+        $atLeastOneSideEqualBoxSide = $actualWidth == $expectedWidth || $actualHeight == $expectedHeight;
+        $noSideOverlaps = $actualWidth >= $expectedWidth && $actualHeight <= $expectedHeight;
+        
+        if (!$atLeastOneSideEqualBoxSide || !$noSideOverlaps) {
+            $this->fail('Image does not fill the given box (' . $expectedWidth . 
+                    ' x ' . $expectedHeight .'). Actual image dimenstions: ' . 
+                    $actualWidth . ' x ' . $actualHeight . '.');
+        }
+    }
+    
+    public function assertImageSizeEquals($expectedWidth, $expectedHeight, $imagePath)
+    {
+        list($actualWidth, $actualHeight) = $this->getImageSize($imagePath);
+        
+        $this->assertEquals($expectedWidth, $actualWidth);
+        $this->assertEquals($expectedHeight, $actualHeight);
+    }
+    
+    public function getImageSize($imagePath) 
+    {
+        $img = imagecreatefromjpeg($imagePath);
+        $actualWidth = imagesx($img);
+        $actualHeight = imagesy($img);
+        imagedestroy($img);    
+        
+        return array($actualWidth, $actualHeight);
     }
 }
--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL | E_STRICT);
 
 define('TESTING_BASE_DIR', dirname(__FILE__));
-define('TESTING_ASSETS_DIR', TESTING_BASE_DIR . '/_files/');
+define('TESTING_ASSETS_DIR', TESTING_BASE_DIR . '/_assets/');
 define('TESTING_TMP_DIR', TESTING_BASE_DIR . '/_tmp/');
 
 require '../vendor/autoload.php';
