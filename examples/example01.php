9968c48b738ef8a1d3e5445af71f43a474648eb9
Andreas Baumgart <andreas@polycast.de>
2012-11-10 22:26:07 +0100

    Applied new namespace to examples.

--- examples/example01.php
+++ examples/example01.php
@@ -5,10 +5,10 @@ require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "setup.php";
 
 require_once 'Zend/Filter/ImageSize.php';
 
-$filter = new Zend_Filter_ImageSize();
+$filter = new Polycast_Filter_ImageSize();
 $output = $filter->setHeight(100)
     ->setWidth(200)
-    ->setOverwriteMode(Zend_Filter_ImageSize::OVERWRITE_ALL)
+    ->setOverwriteMode(Polycast_Filter_ImageSize::OVERWRITE_ALL)
     ->filter('./rick.jpg');
 
 echo "Output saved to: $output" . PHP_EOL;
